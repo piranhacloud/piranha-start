ea45cabbdc7a0ca4c56ce1867f5f81ce9fae7c91
Manfred Riem <mriem@manorrock.com>
2023-06-22 16:14:58 -0500

    Fixes issue #64 - Add test for Java 17 selection (#65)

---
+++ src/test/java/start/JavaVersionIT.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2002-2023 Manorrock.com. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package start;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The start page integration test.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class JavaVersionIT {
+
+    private static Playwright playwright;
+
+    @AfterAll
+    public static void afterAll() {
+        playwright.close();
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        playwright = Playwright.create();
+    }
+
+    /**
+     * Test the 'Java 17' choice.
+     *
+     * @throws Exception when an error occurs.
+     */
+    @Test
+    public void testJava17() throws Exception {
+        List<BrowserType> browserTypes = Arrays.asList(
+                playwright.chromium(),
+                playwright.firefox(),
+                playwright.webkit()
+        );
+        for (BrowserType browserType : browserTypes) {
+            try (Browser browser = browserType.launch()) {
+                BrowserContext context = browser.newContext();
+                Page page = context.newPage();
+                page.navigate("http://localhost:8080/piranha-start/start.xhtml");
+                page.getByText("17").click();
+                page.onDownload((download) -> {
+                    assertEquals("project.zip", download.suggestedFilename());
+                });
+                page.getByText("Download").click();
+            }
+        }
+    }
+
+    /**
+     * Test the 'Java 19' choice.
+     *
+     * @throws Exception when an error occurs.
+     */
+    @Test
+    public void testJava19() throws Exception {
+        List<BrowserType> browserTypes = Arrays.asList(
+                playwright.chromium(),
+                playwright.firefox(),
+                playwright.webkit()
+        );
+        for (BrowserType browserType : browserTypes) {
+            try (Browser browser = browserType.launch()) {
+                BrowserContext context = browser.newContext();
+                Page page = context.newPage();
+                page.navigate("http://localhost:8080/piranha-start/start.xhtml");
+                page.getByText("19").click();
+                page.onDownload((download) -> {
+                    assertEquals("project.zip", download.suggestedFilename());
+                });
+                page.getByText("Download").click();
+            }
+        }
+    }
+}
