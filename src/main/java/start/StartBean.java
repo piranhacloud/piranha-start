89583de1e57dba5217416ca3e86a6fba53223dca
Manfred Riem <mriem@manorrock.com>
2024-10-29 07:42:20 -0500

    Fixes #190 - Update to Java 23 (#192)

--- src/main/java/start/StartBean.java
+++ src/main/java/start/StartBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2023 Manorrock.com. All Rights Reserved.
+ * Copyright (c) 2002-2024 Manorrock.com. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -188,7 +188,7 @@ public class StartBean implements Serializable {
     public SelectItem[] getJavaVersions() {
         return new SelectItem[]{
             new SelectItem("21", "21"),
-            new SelectItem("22", "22")
+            new SelectItem("23", "23")
         };
     }
 
